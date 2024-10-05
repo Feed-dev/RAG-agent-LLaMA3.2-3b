f5d604f25ddb7354a19a5e04b01f247ebce0d7ef
Feed-dev <federico.lubbert@gmail.com>
2024-10-05 22:56:34 +0200

    updated

--- utils/state.py
+++ utils/state.py
@@ -5,7 +5,9 @@ class GraphState(TypedDict):
     question: str
     context: List[str]
     current_step: str
-    final_answer: Optional[str]
+    final_answer: str
     retriever: Any
     web_search_tool: Any
     error: Optional[str]
+    selected_namespaces: List[str]
+    web_search_results: List[str]
